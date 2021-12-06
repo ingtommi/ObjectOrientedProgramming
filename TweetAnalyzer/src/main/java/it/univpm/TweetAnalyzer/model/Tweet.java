a56c76f2a20da1ec05e878ef133ff5fe820a5103
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-06 16:51:34 +0100

    added tweet class

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
@@ -2,4 +2,43 @@ package it.univpm.TweetAnalyzer.model;
 
 public class Tweet {
 
+
+	public Tweet() {
+        this.created_at = null;
+		this.id = 0;
+		this.text = "";
+	}
+
+
+
+	private String created_at;
+    private long id;
+    private String text;
+
+
+
+	public String getCreated_at() {
+		return created_at;
+	}
+
+	public void setCreated_at(String created_at) {
+		this.created_at = created_at;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+
 }
