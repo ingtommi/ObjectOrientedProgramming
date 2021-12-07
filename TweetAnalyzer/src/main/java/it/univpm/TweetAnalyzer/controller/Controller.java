057fd996042e36a2d64b2275e341742631620aac
Tommaso <tfava00@gmail.com>
2021-12-07 16:21:31 +0100

    update

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/controller/Controller.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/controller/Controller.java
@@ -10,10 +10,24 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class Controller {
 	
-	@Autowired
+	//@Autowired
 	
-	@GetMapping(value = "/tweet/data")
-	public ResponseEntity<Object> getData (@RequestParam(name = "query") String query, @RequestParam(name = "count", defaultValue = "10") int count) {
-		return new ResponseEntity<>(.getData(), HttpStatus.OK);
+	//@GetMapping(value = "/tweet/metadata")
+	
+	//@GetMapping(value = "/tweet/data")
+	
+	@GetMapping(value = "/tweet/filter/hashtag")
+	public ResponseEntity<Object> getData(@RequestParam(name = "query") String query,
+			@RequestParam(name = "count", defaultValue = "10") int count, 
+			@RequestParam(name = "lang", defaultValue = "it") String lang) {
+		//TODO: modellare classe chiamata
+		return new ResponseEntity<>(null, HttpStatus.OK);
+	}
+	
+	@GetMapping(value = "/tweet/filter/geo")
+	public ResponseEntity<Object> filter(@RequestParam(name = "long") String lon, @RequestParam(name = "lat") String lat, 
+			@RequestParam(name = "rad") String rad, @RequestParam(name = "count", defaultValue = "10") int count) {
+		//TODO: modellare classe chiamata
+		return new ResponseEntity<>(null, HttpStatus.OK);
 	}
 }
