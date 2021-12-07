a8f1238963b03f84dfd1c083a77f17b169bf9883
Tommaso <tfava00@gmail.com>
2021-12-07 11:29:36 +0100

    added new attributes

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
@@ -1,17 +1,24 @@
 package it.univpm.TweetAnalyzer.model;
 
+import java.util.ArrayList;
+
 public class Tweet {
 
 	private String created_at;
 	private long id;
 	private String text;
-
-	public Tweet() {
-		this.created_at = null;
-		this.id = 0;
-		this.text = "";
+	private ArrayList<String> hashtags;
+	private String[] coordinates; //[long,lat]
+	private String[] place; //[full_name,country_code]
+	
+	public Tweet(String created_at, long id, String text, ArrayList<String> hashtags, String[] coordinates, String[] place) {
+		this.created_at = created_at;
+		this.id = id;
+		this.text = text;
+		this.hashtags = hashtags;
+		this.coordinates = coordinates;
+		this.place = place;
 	}
-
 
 	public String getCreated_at() {
 		return created_at;
@@ -37,4 +44,27 @@ public class Tweet {
 		this.text = text;
 	}
 
+	public ArrayList<String> getHashtags() {
+		return hashtags;
+	}
+
+	public void setHashtags(ArrayList<String> hashtags) {
+		this.hashtags = hashtags;
+	}
+
+	public String[] getCoordinates() {
+		return coordinates;
+	}
+
+	public void setCoordinates(String[] coordinates) {
+		this.coordinates = coordinates;
+	}
+
+	public String[] getPlace() {
+		return place;
+	}
+
+	public void setPlace(String[] place) {
+		this.place = place;
+	}
 }
