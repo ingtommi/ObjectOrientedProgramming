d8fc3bec44643447e392e5896120768d49dce5f0
Tommaso <tfava00@gmail.com>
2021-12-08 11:59:56 +0100

    revision

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/controller/Controller.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/controller/Controller.java
@@ -7,27 +7,31 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import it.univpm.TweetAnalyzer.service.APICall;
+
 @RestController
 public class Controller {
 	
-	//@Autowired
+	@Autowired
+	APICall apicall;
 	
-	//@GetMapping(value = "/tweet/metadata")
+	//TODO: modellare classi chiamate al posto di NULL
 	
-	//@GetMapping(value = "/tweet/data")
+	@GetMapping(value = "/tweet/metadata")
+	public ResponseEntity<Object> getMeta() {
+		return new ResponseEntity<>(apicall.getMeta(), HttpStatus.OK);
+	}
 	
-	@GetMapping(value = "/tweet/filter/hashtag")
-	public ResponseEntity<Object> getData(@RequestParam(name = "query") String query,
-			@RequestParam(name = "count", defaultValue = "10") int count, 
+	@GetMapping(value = "/tweet/data")
+	public ResponseEntity<Object> getData(@RequestParam(name = "hashtags") String query,
+			@RequestParam(name = "count", defaultValue = "5") int count, 
 			@RequestParam(name = "lang", defaultValue = "it") String lang) {
-		//TODO: modellare classe chiamata
-		return new ResponseEntity<>(null, HttpStatus.OK);
+		return new ResponseEntity<>(apicall.getData(), HttpStatus.OK);
 	}
 	
 	@GetMapping(value = "/tweet/filter/geo")
 	public ResponseEntity<Object> filter(@RequestParam(name = "long") String lon, @RequestParam(name = "lat") String lat, 
-			@RequestParam(name = "rad") String rad, @RequestParam(name = "count", defaultValue = "10") int count) {
-		//TODO: modellare classe chiamata
+			@RequestParam(name = "rad") String rad) {
 		return new ResponseEntity<>(null, HttpStatus.OK);
 	}
 }
