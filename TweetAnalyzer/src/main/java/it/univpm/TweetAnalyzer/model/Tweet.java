2a3e7f269b317f2033643f3a6139fd1222af96b5
Tommaso <tfava00@gmail.com>
2021-12-09 10:48:47 +0100

    corrected error

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
@@ -6,7 +6,7 @@ public class Tweet extends Model {
 
 	private ArrayList<String> hashtags;
 	
-	public Tweet(String created_at, long id, String location, String text, ArrayList<String> hashtags) {
+	public Tweet(String created_at, long id, String location, ArrayList<String> hashtags) {
 		super(created_at,id,location);
 		this.hashtags = hashtags;
 	}
