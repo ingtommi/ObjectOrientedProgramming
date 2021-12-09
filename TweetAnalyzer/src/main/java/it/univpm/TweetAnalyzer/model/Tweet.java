b7c9667c46b566dfebd530429a98310ec49d9de2
Tommaso <tfava00@gmail.com>
2021-12-09 10:48:08 +0100

    removed text attribute

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
@@ -4,21 +4,11 @@ import java.util.ArrayList;
 
 public class Tweet extends Model {
 
-	private String text;
 	private ArrayList<String> hashtags;
 	
 	public Tweet(String created_at, long id, String location, String text, ArrayList<String> hashtags) {
 		super(created_at,id,location);
-		this.text = text;
 		this.hashtags = hashtags;
-	}
-
-	public String getText() {
-		return text;
-	}
-
-	public void setText(String text) {
-		this.text = text;
 	}
 
 	public ArrayList<String> getHashtags() {
