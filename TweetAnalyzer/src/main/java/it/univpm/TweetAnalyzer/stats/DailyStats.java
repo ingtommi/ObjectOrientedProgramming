46d39b4709ca0bee3123c4a591c5bd239e4f1237
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-10 19:12:38 +0100

    update stats

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/stats/DailyStats.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/stats/DailyStats.java
@@ -1,5 +1,17 @@
 package it.univpm.TweetAnalyzer.stats;
 
-public class DailyStats {
+import java.time.LocalDate;
+import java.util.ArrayList;
 
+import it.univpm.TweetAnalyzer.model.Tweet;
+
+public class DailyStats {
+	
+	private LocalDate date;
+	private ArrayList<Tweet> tweets;
+	
+	public DailyStats (LocalDate date, ArrayList<Tweet> tweets) {
+		this.date = date;
+		this.tweets = tweets;
+	}
 }
--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/stats/GeoStats.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/stats/GeoStats.java
@@ -1,5 +1,20 @@
 package it.univpm.TweetAnalyzer.stats;
 
+import java.util.ArrayList;
+
+import it.univpm.TweetAnalyzer.model.*;
+
+
 public class GeoStats {
+ 
+	private String location;
+	private ArrayList<Tweet> tweets;
+	private ArrayList<User> users;
+
+	public GeoStats (String location , ArrayList<Tweet> tweets, ArrayList<User> users) {
+		this.location = location;
+		this.tweets = tweets;
+		this.users = users;
+	}
 
 }
