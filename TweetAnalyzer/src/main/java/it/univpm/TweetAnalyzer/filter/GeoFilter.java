e51f184a8476a0426489ee03d875a0ee81f80cf3
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-10 19:12:47 +0100

    update filter

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/filter/GeoFilter.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/filter/GeoFilter.java
@@ -1,5 +1,19 @@
 package it.univpm.TweetAnalyzer.filter;
 
-public class GeoFilter {
+import java.util.ArrayList;
 
+import it.univpm.TweetAnalyzer.model.*;
+
+
+public class GeoFilter {
+	
+	private String location;
+	private ArrayList<Tweet> tweets;
+	private ArrayList<User> users;
+
+	public GeoFilter (String location , ArrayList<Tweet> tweets, ArrayList<User> users) {
+		this.location = location;
+		this.tweets = tweets;
+		this.users = users;
+	}
 }
