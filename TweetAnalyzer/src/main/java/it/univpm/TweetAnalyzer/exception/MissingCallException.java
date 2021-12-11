af513fc1dddb49efd6422f12970f02feb58555d5
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-11 17:00:27 +0100

    exception

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/exception/MissingCallException.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/exception/MissingCallException.java
@@ -1,15 +1,16 @@
 package it.univpm.TweetAnalyzer.exception;
 
-public class MissParametersException extends Exception {
-
+public class MissingCallException extends Exception{
+	
 	private static final long serialVersionUID = 1L;
 
-	public MissParametersException() {
+	public MissingCallException() {
 		super();
 	}
 
 	//ritorna messaggio di errore
 	public String getErr() {
-		return "ERRORE: Alcuni paramentri non sono presenti!";
+		return "ERRORE: You have to contact http://localhost:8080/tweet/get/{method} ";
 	}
+
 }
