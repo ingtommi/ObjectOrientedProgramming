7dde8169ada364efb5363149b1f00e92eb533ea2
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-11 16:14:37 +0100

    list

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/service/APICall.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/service/APICall.java
@@ -17,17 +17,17 @@ import org.json.simple.JSONValue;
 import org.json.simple.parser.ParseException;
 
 public class APICall {
-	
+
 	private static final String apiBase = "https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?";
 	private String api;
 	private String ht1,ht2,ht3;
 	private int count;
 	private String lang;
 	private String met;
-	
+
 	ArrayList<Tweet> tweets = new ArrayList<Tweet>();
 	ArrayList<User> users = new ArrayList<User>();
-	
+
 	public APICall(String ht1, String ht2, String ht3, String met, String lang, int count) {
 		this.ht1 = ht1.replace("#","%23").replaceAll("\\s+","");
 		if(ht2!=null) {
@@ -40,19 +40,19 @@ public class APICall {
 		this.count = count;
 		this.lang = lang;
 	}
-	
+
 	public ArrayList<Tweet> getTweets() { return tweets; }
 	public ArrayList<User> getUsers() { return users; }
-	
+
 	public String apiBuild() {
-		
+
 		if(met.equals("AND") || met.equals("OR")) {
 			if(ht2==null && ht3==null) {
 				api = apiBase + "q=" + ht1 + "&count=" + count +"&lang=" + lang;
 			}
 			else if(ht3==null) {
 				api = apiBase + "q=" + ht1 + "%20" + met + "%20" + ht2 + "&count=" + count +"&lang=" + lang;
-				
+
 			}
 			else if(ht2==null) {
 				api = apiBase + "q=" + ht1 + "%20" + met + "%20" + ht3 + "&count=" + count +"&lang=" + lang;
@@ -64,14 +64,14 @@ public class APICall {
 		//TODO: inserire eccezione se metodo diverso da AND o OR
 		return api;
 	}
-	
+
 	@SuppressWarnings("unchecked")
-	public saveData() {
-		
+	public ArrayList<Object> saveData() {
+
 		String body = "";
 		String line = "";
 		JSONObject obj = null;
-		
+
 		try {
 			URLConnection openConnection = new URL(this.apiBuild()).openConnection();
 			InputStream in = openConnection.getInputStream();
@@ -86,40 +86,48 @@ public class APICall {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		//TODO: lanciare eccezione se status/hashtags = []
 		JSONArray statuses = (JSONArray) obj.get("statuses");
 		for(int i=0; i<statuses.size(); i++) {
 			JSONObject tweet = (JSONObject) statuses.get(i);
 			//TWEET INFO
 			String tweetDate = (String) tweet.get("created_at");
-            Long tweetId = (Long) tweet.get("id");
-            String tweetLocation = (String) tweet.get("place");
-  
-            JSONObject entities = (JSONObject) tweet.get("entities");
-            JSONArray ht = (JSONArray) entities.get("hashtags");
-            ArrayList<String> hashtags = new ArrayList<String>();
-            for(int j=0; j<ht.size(); j++) {
-            	JSONObject hashtag = (JSONObject) ht.get(j);
-            	String text = (String) hashtag.get("text");
-            	hashtags.add(text);
-            }
-            
-            Tweet tw = new Tweet(tweetDate,tweetId,tweetLocation,hashtags);
-            tweets.add(tw);
-            
-            //USER INFO
-            JSONObject user = (JSONObject) tweet.get("user");
-            String userDate = (String) user.get("created_at");
-            Long userId = (Long) user.get("id");
-            String userLocation = (String) user.get("location");
-            //imposto a null per avere uniformità con tweetLocation
-            if(userLocation.equals(""))
-            	userLocation = null;
-            String userName = (String) user.get("name");
-            
-            User us = new User(userDate,userId,userLocation,userName);
-            users.add(us);
-        }
-	}
+			Long tweetId = (Long) tweet.get("id");
+			String tweetLocation = (String) tweet.get("place");
+
+			JSONObject entities = (JSONObject) tweet.get("entities");
+			JSONArray ht = (JSONArray) entities.get("hashtags");
+			ArrayList<String> hashtags = new ArrayList<String>();
+			for(int j=0; j<ht.size(); j++) {
+				JSONObject hashtag = (JSONObject) ht.get(j);
+				String text = (String) hashtag.get("text");
+				hashtags.add(text);
+			}
+
+			Tweet tw = new Tweet(tweetDate,tweetId,tweetLocation,hashtags);
+			tweets.add(tw);
+
+			//USER INFO
+			JSONObject user = (JSONObject) tweet.get("user");
+			String userDate = (String) user.get("created_at");
+			Long userId = (Long) user.get("id");
+			String userLocation = (String) user.get("location");
+			//imposto a null per avere uniformità con tweetLocation
+			if(userLocation.equals(""))
+				userLocation = null;
+			String userName = (String) user.get("name");
+
+			User us = new User(userDate,userId,userLocation,userName);
+			users.add(us);
+		}
+
+
+		ArrayList<Object> list = new ArrayList<Object>();
+
+		list.addAll(tweets);
+		list.addAll(users);
+
+		return list;
+		}
 }
