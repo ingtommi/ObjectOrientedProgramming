3fb1d3911da694c0974cdcb2122fc3c19a5e2475
Tommaso <tfava00@gmail.com>
2021-12-12 16:53:53 +0100

    added new output

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/stats/HashStats.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/stats/HashStats.java
@@ -29,18 +29,18 @@ public class HashStats {
 	//se hashtag != null
 	private HashMap<String, Float> specific() {
 		
-		int count = 0;
+		int tot = 0;
 		for(Tweet tweet : tweets) {
 			for(String hash : tweet.getHashtags()) {
 				if(hash.toLowerCase().equals(hashtag.toLowerCase())) { //permette di trovare stringhe scritte diversamente
-					count++;
+					tot++;
 				} 
 			}	
 		}
 		HashMap<String, Float> spe = new HashMap<String, Float>();
 		spe.put("Total tweets downloaded", (float) tweets.size());
-		spe.put("Tweets containing #" + hashtag, (float) count);
-		spe.put("Percentage", 100*(float) count/tweets.size());
+		spe.put("Tweets containing #" + hashtag, (float) tot);
+		spe.put("Percentage", 100*(float) tot/tweets.size());
 
 		return spe;
 	}
@@ -48,14 +48,24 @@ public class HashStats {
 	//se hashthag == null
 	private HashMap<String, Float> general() {
 		
-		int count = 0;
+		int tot = 0;
+		int max = 0;
+		int min = 100;
 		for(Tweet tweet : tweets) {
-			count += tweet.getHashtags().size();
+			tot += tweet.getHashtags().size();
+			if(tweet.getHashtags().size()>max) {
+				max = tweet.getHashtags().size();
+			}
+			if(tweet.getHashtags().size()<min) {
+				min = tweet.getHashtags().size();
+			}
 		}
 		HashMap<String, Float> gen = new HashMap<String, Float>();
 		gen.put("Total tweets downloaded", (float) tweets.size());
-		gen.put("Average hashtags per tweet", (float) count/tweets.size());
-
+		gen.put("Max hashtags per tweet", (float) max);
+		gen.put("Min hashtags per tweet", (float) min);
+		gen.put("Average hashtags per tweet", (float) tot/tweets.size());
+		
 		return gen;
 	}
 }
