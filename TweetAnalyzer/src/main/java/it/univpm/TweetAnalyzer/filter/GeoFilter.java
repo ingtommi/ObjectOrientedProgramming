ededfb4ed76c651b1c16d577e27e6bfc5cb06d7f
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-13 15:55:01 +0100

    update GeoFilter class

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/filter/GeoFilter.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/filter/GeoFilter.java
@@ -2,11 +2,14 @@ package it.univpm.TweetAnalyzer.filter;
 
 import java.util.ArrayList;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
 import it.univpm.TweetAnalyzer.model.*;
 
 
 public class GeoFilter {
-	
+
 	private String location;
 	private ArrayList<Tweet> tweets;
 	private ArrayList<User> users;
@@ -16,4 +19,44 @@ public class GeoFilter {
 		this.tweets = tweets;
 		this.users = users;
 	}
+
+	@SuppressWarnings("unchecked")
+	public JSONObject geofilter() {
+
+		JSONObject loc = new JSONObject();
+		JSONArray list = new JSONArray();
+
+		for(int i=0; i<tweets.size(); i++) {
+			JSONObject tweet = null;
+			JSONObject user = null;
+			if(tweets.get(i).getLocation() != null) {
+				if(tweets.get(i).getLocation().toLowerCase().contains(location.toLowerCase())) {
+					tweet = new JSONObject();
+					tweet.put("created_at",tweets.get(i).getDatatime());
+					tweet.put("id",tweets.get(i).getId());
+					tweet.put("location",tweets.get(i).getLocation());
+					tweet.put("hashtags",tweets.get(i).getHashtags());
+				}	
+				if(tweet != null) {
+					list.add(tweet);
+				}
+				loc.put("tweets written in " + location, list);
+			}
+			else if(tweets.get(i).getLocation() == null && users.get(i).getLocation() != null) {
+				if(users.get(i).getLocation().toLowerCase().contains(location.toLowerCase())) {
+					user = new JSONObject();
+					user.put("location",users.get(i).getLocation());
+				}
+				if (user!= null) {
+					list.add(user);
+				}
+				loc.put("tweets written in " + location, list);
+			}
+
+		}
+		return loc;
+	}
+
 }
+
+
