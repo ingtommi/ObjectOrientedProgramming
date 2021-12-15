8693c5ab204b0450608929bd06ccada8a53eb169
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-15 09:30:42 +0100

    javadoc application

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/TweetAnalyzerApplication.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/TweetAnalyzerApplication.java
@@ -3,9 +3,19 @@ package it.univpm.TweetAnalyzer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Classe che avvia l'applicazione Spring
+ * @author Marco Ciampichetti
+ * @author Tommaso Fava
+ *
+ */
 @SpringBootApplication
 public class TweetAnalyzerApplication {
 
+	/**
+	 * Metodo main
+	 * @param args indica gli argomenti del main
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(TweetAnalyzerApplication.class, args);
 	}
