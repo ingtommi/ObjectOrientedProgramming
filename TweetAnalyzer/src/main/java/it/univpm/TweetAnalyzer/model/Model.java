fd4cc012843ebfb2e7c5868f7fb51374e8dce540
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-15 11:46:13 +0100

    javadoc Model

--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Model.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Model.java
@@ -2,19 +2,58 @@ package it.univpm.TweetAnalyzer.model;
 
 import java.time.LocalDate;
 
+/**
+ * Questa classe rappresenta il modello che si ottiene effettuando una ricerca 
+ * Si hanno dei parametri comuni di Tweet e User che sono stati raccolti in questa classe
+ * @author Marco Ciampichetti
+ * @author Tommaso Fava
+ */
+
 public class Model {
-	
+
+	/**
+	 * data
+	 */
 	private LocalDate date;
+
+	/**
+	 * numero identificativo
+	 */
+
 	private long id;
+
+	/**
+	 * luogo
+	 */
 	private String location;
-	
+
+	/**
+	 * Questo è il costruttore
+	 * @param date sta ad indicare la data
+	 * @param id sta ad indicare il numero di riconoscimento
+	 * @param location sta ad indicare il luogo 
+	 */
 	public Model(LocalDate date, long id, String location) {
 		this.date = date;
 		this.id = id;
 		this.location = location;
 	}
 
+	/**
+	 * Metodo che resituisce la data in formato LocalDate
+	 * @return date
+	 */
 	public LocalDate getDatatime() { return date; }
+
+	/**
+	 * Metodo che restituisce il numero identificativo come long
+	 * @return id
+	 */
 	public long getId() { return id; }
+
+	/**
+	 * Metodo che restituisce il luogo come stringa
+	 * @return location
+	 */
 	public String getLocation() { return location; }
 }
--- TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
+++ TweetAnalyzer/src/main/java/it/univpm/TweetAnalyzer/model/Tweet.java
@@ -3,14 +3,35 @@ package it.univpm.TweetAnalyzer.model;
 import java.time.LocalDate;
 import java.util.ArrayList;
 
+/**
+ * Questa è una classe che estende Model e 
+ * modella il tweet che si ottiene nella ricerca
+ * @author Marco Ciampichetti
+ * @author Tommaso Fava
+ */
+
 public class Tweet extends Model {
 
+	/**
+	 * ArrayList di hashtag
+	 */
 	private ArrayList<String> hashtags;
-	
+
+	/**
+	 * Questo è il costruttore
+	 * @param date sta ad indicare la data del tweet
+	 * @param id sta ad indicare il numero identificativo del tweet
+	 * @param location sta ad indicare la posizione in cui è stato scritto il tweet
+	 * @param hashtags sta ad indicare la lista degli hashtag contenuti nel tweet
+	 */
 	public Tweet(LocalDate date, long id, String location, ArrayList<String> hashtags) {
 		super(date,id,location);
 		this.hashtags = hashtags;
 	}
 
+	/**
+	 * Metodo che restituisce una lista di tutti gli hashtag di un tweet
+	 * @return hashtag
+	 */
 	public ArrayList<String> getHashtags() { return hashtags; }
 }
