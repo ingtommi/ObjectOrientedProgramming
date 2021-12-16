d885780944d3f8463bab57bccef2a6e4e043e5a5
MarcoCiamp <s1092617@studenti.univpm.it>
2021-12-16 14:54:44 +0100

    javadoc Tests update

--- TweetAnalyzer/src/test/java/it/univpm/TweetAnalyzer/tests/Tests.java
+++ TweetAnalyzer/src/test/java/it/univpm/TweetAnalyzer/tests/Tests.java
@@ -14,15 +14,21 @@ import org.springframework.boot.test.context.SpringBootTest;
 import it.univpm.TweetAnalyzer.exception.WrongMethodException;
 import it.univpm.TweetAnalyzer.service.APICall;
 
+/**
+ * Questa classe contiene dei test per verificare il corretto funzionamento di alcune parti del programma
+ * @author Marco Ciampichetti
+ * @author Tommaso Fava
+ */
+
 @SpringBootTest
 class Tests {
-	
+
 	private APICall c1,c2;
 	private String created_at;
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
-		
+
 		c1 = new APICall("#univpm",null,null,"and","it",5);
 		c2 = new APICall("#java",null,null,"xxx","en",10);
 		created_at = "Sun Feb 25 18:11:01 +0000 2018";
@@ -30,26 +36,36 @@ class Tests {
 
 	@AfterEach
 	void tearDown() throws Exception {}
-	
+
+	/**
+	 * Questo test verifica la correttezza dell'url
+	 * @throws WrongMethodException se i metodi inseriti sono diversi da AND o OR
+	 */
 	@Test
 	@DisplayName("Controllo correttezza url")
 	void analyze1() throws WrongMethodException {
-		
+
 		assertEquals(c1.apiBuild(),"https://wd4hfxnxxa.execute-api.us-east-2.amazonaws.com/dev/api/1.1/search/tweets.json?q=%23univpm&count=5&lang=it");
 	}
-	
+
+	/**
+	 * Questo test verifica la correttezza della data
+	 */
 	@Test
 	@DisplayName("Controllo correttezza data")
 	void analyze2() {
-		
+
 		LocalDate date = LocalDate.of(2018,02,25);
 		assertEquals(c1.parseData(created_at),date);
 	}
-	
+
+	/**
+	 * Questo test verifica il corretto lancio dell'eccezione WrongMethodException
+	 */
 	@Test
 	@DisplayName("Controllo lancio eccezione WrongMethodException")
 	void analyze3() {
-		
+
 		try {
 			c2.apiBuild();
 			//serve per generare un failure
